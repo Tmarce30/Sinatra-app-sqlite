9141f0a56859df8c5d8dd3b294f9e6f073f917a9
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-05 13:19:38 +0100

    created artist page and implemented get to display albums of the artist on the page

--- lib/app.rb
+++ lib/app.rb
@@ -6,15 +6,15 @@ DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'
 
 get "/" do
   # TODO: Gather all artists to be displayed on home page
-  @artists = DB.execute('SELECT id, name FROM artists').sort
+  @artists = DB.execute('SELECT name, id FROM artists').sort
   erb :home # Will render views/home.erb file (embedded in layout.erb)
 end
 
 get "/artist/:id" do
   DB.results_as_hash = false
-  @albums = DB.execute('SELECT albums.id, albums.title FROM albums
+  @albums = DB.execute('SELECT albums.title, albums.id FROM albums
                         JOIN artists ON artists.id = albums.artist_id
-                        WHERE artist_id = ?', params[:id].to_i)
+                        WHERE artist_id = ?', params[:id].to_i).sort
   @artist_name = DB.execute('SELECT name FROM artists
                              WHERE id = ?', params[:id].to_i).flatten[0]
   erb :artist
